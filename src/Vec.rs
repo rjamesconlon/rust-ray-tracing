797999751490fc65e337fb546fccf5ece6d220cf
rjamesconlon <rjamesconlon@gmail.com>
2025-02-10 20:34:43 +0000

    more vector functions

--- src/Vec.rs
+++ src/Vec.rs
@@ -1,5 +1,6 @@
 use std::ops;
 
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub struct Vec {
   pub x: f64,
   pub y: f64,
@@ -14,6 +15,18 @@ impl Vec {
   pub fn get_vector(&self) -> (f64, f64, f64) {
     (self.x, self.y, self.z)
   }
+
+  pub fn length_squared(&self) -> f64 {
+    (self.x * self.x) + (self.y * self.y) + (self.z * self.z)
+  }
+
+  pub fn length(&self) -> f64 {
+    self.length_squared().sqrt()
+  }
+
+  pub fn dot(&self, v: &Vec) -> f64 {
+    (self.x * v.x) + (self.y * v.y) + (self.z * v.z)
+  }
 }
 
 impl ops::Add for Vec {
@@ -24,6 +37,42 @@ impl ops::Add for Vec {
       x: self.x + v.x,
       y: self.y + v.y,
       z: self.z + v.z,
+    }
+  }
+}
+
+impl ops::Sub for Vec {
+  type Output = Vec;
+
+  fn sub(self, v: Vec) -> Vec {
+    Vec {
+      x: self.x - v.x,
+      y: self.y - v.y,
+      z: self.z - v.z,
+    }
+  }
+}
+
+impl ops::Mul<f64> for Vec {
+  type Output = Vec;
+
+  fn mul(self, i: f64) -> Vec {
+    Vec {
+      x: self.x * i,
+      y: self.y * i,
+      z: self.z * i,
+    }
+  }
+}
+
+impl ops::Div<f64> for Vec {
+  type Output = Vec;
+
+  fn div(self, i: f64) -> Vec {
+    Vec {
+      x: self.x / i,
+      y: self.y / i,
+      z: self.z / i,
     }
   }
 }
